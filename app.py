7eb2f67b87d424e7c9fd083e8024133900679f1f
Samarth Verma <verma.samarth05@gmail.com>
2025-01-08 20:26:54 -0500

    Initial

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to my Flask app!"
+    return render_template('index.html')
+
+@app.route('/parameters', methods=['POST'])
+def parameters():
+    selected_platform = request.form['platform']
+    return render_template('parameters.html', platform=selected_platform)
 
 if __name__ == '__main__':
     app.run(debug=True)
