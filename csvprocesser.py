bdba1ef30bc8de9d62d678f0222f1d55124c70ed
Samarth Verma <verma.samarth05@gmail.com>
2025-01-23 22:05:41 -0500

    defined parameters within code

--- csvprocesser.py
+++ csvprocesser.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 # structure of csv 
 # email , [parameter1], [parameter2], ...
@@ -6,3 +7,19 @@ import csv
 
 # manual input: 
 # user email, email key, subject, body
+
+def main():
+    with open('input.csv', 'r') as file:
+        csv_reader = csv.reader(file)
+        headers = next(csv_reader)
+        parameters = headers[1:] # all the parameters are here
+
+
+        # Process each row
+        for row in csv_reader:
+            if len(row) > 0:  # Check if row is not empty
+                email = row[0]  # First column is email
+                parameters = row[1:]  # Remaining columns are parameters
+
+if __name__ == "__main__": 
+    main()
